4ebffb365c351db2589cb4570c824898feb59c58
vaibhavdhanaliya14 <vaibhavdhanaliya14@gmail.com>
2026-08-13 04:37:46 +0530

    feat: add auth and profile foundation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,62 @@
+import {
+  Injectable,
+  ConflictException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from '../users/entities/user.entity';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { Role } from '../doctor/dto/role.enum';
 
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async signup(email: string, pass: string, role: Role) {
+    const existingUser = await this.userRepository.findOne({
+      where: { email },
+    });
+    if (existingUser) {
+      throw new ConflictException('Email already exists');
+    }
+
+    const hashedPassword = await bcrypt.hash(pass, 10);
+
+    const user = this.userRepository.create({
+      email,
+      pass: hashedPassword,
+      role,
+    });
+
+    await this.userRepository.save(user);
+    return {
+      message: 'User registered successfully',
+      userId: user.id,
+      email: user.email,
+      role: user.role,
+    };
+  }
+
+  async login(email: string, pass: string) {
+    const user = await this.userRepository.findOne({ where: { email } });
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPasswordValid = await bcrypt.compare(pass, user.pass);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const payload = { sub: user.id, email: user.email, role: user.role };
     return {
       access_token: this.jwtService.sign(payload),
     };
   }
-
+}
